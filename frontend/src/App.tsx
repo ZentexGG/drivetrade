a4eb517a7e4eb4fbd7a76cb1626c6a4e8aedf048
ZentexGG <stefan.tanase2121@gmail.com>
2023-05-24 23:56:27 +0300

    initial files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Button} from "react-bootstrap";
+export default function App() {
+  return (
+    <div className="App">
+      <Button>ABC</Button>
+    </div>
+  );
+}
