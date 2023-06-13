fccbadd9772892583ddb41d00cafd168b37709d8
ZentexGG <stefan.tanase2121@gmail.com>
2023-06-13 12:09:05 +0300

    Created controller & some of the routes

---
+++ backend/PresentationLayer/Controllers/VehicleController.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Interfaces;
+using DataLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers;
+[ApiController]
+[Route("api/vehicle")]
+public class VehicleController : ControllerBase
+{
+    private readonly IVehicleService _service;
+
+    public VehicleController(IVehicleService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public IActionResult GetAllVehicles()
+    {
+        var vehicles = _service.GetAll();
+        return Ok(vehicles);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetVehicleById(int id)
+    {
+        try
+        {
+            var vehicle = _service.GetById(id);
+            return Ok(vehicle);
+        }
+        catch (KeyNotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+
+    [HttpPost]
+    public IActionResult CreateVehicle(Vehicle vehicle)
+    {
+        try
+        {
+            _service.Create(vehicle);
+            return CreatedAtAction(nameof(GetVehicleById), new { id = vehicle.ID }, vehicle);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+}
