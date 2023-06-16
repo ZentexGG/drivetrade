1fa250205e8d4d753b77181fa078c0ed9177a9dc
ZentexGG <stefan.tanase2121@gmail.com>
2023-06-16 13:30:39 +0300

    added scoped services for dependency injection

--- backend/PresentationLayer/Program.cs
+++ backend/PresentationLayer/Program.cs
@@ -7,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IVehicleService, VehicleService>();
+builder.Services.AddScoped<IBrandService, BrandService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
