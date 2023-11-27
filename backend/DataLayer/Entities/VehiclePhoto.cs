c42fffc99e1fff803ad4dc8c81f2828e0f0cadcb
ZentexGG <stefan.tanase2121@gmail.com>
2023-11-27 15:34:30 +0200

    changed photo structure

--- backend/DataLayer/Entities/VehiclePhoto.cs
+++ backend/DataLayer/Entities/VehiclePhoto.cs
@@ -8,11 +8,9 @@ public class VehiclePhoto
     [Key] 
     public int ID { get; set; }
 
-    public byte[] Bytes { get; set; }
-    public string Description { get; set; }
-    public string FileExtension { get; set; }
-    public decimal Size { get; set; }
+    public byte[] ImageData { get; set; }
+    public string FileName { get; set; }
+    public DateTime UploadDate { get; set; }
     public int VehicleId { get; set; }
-    [ForeignKey("VehicleId")] 
     public Vehicle Vehicle { get; set; }
 }
