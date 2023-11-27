bae79c6c6b6a1c7f436e9b015e10ff25cb762509
ZentexGG <stefan.tanase2121@gmail.com>
2023-11-27 15:32:23 +0200

    vehicle dto for adding new vehicles

---
+++ backend/BusinessLayer/EntitiesDTOs/VehicleDto.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Http;
+
+namespace BusinessLayer.EntitiesDTOs;
+
+public class VehicleDto
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string? Description { get; set; }
+    public int CategoryId { get; set; }
+    public int BrandId { get; set; }
+    public int ConditionId { get; set; }
+    public int DriveTypeId { get; set; }
+    public int FuelTypeId { get; set; }
+    public int GearboxTypeId { get; set; }
+    public List<IFormFile> Photos { get; set; }
+}
