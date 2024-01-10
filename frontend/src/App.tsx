690a68da447a2c8bcecb57e2580e5dc4836c7737
ZentexGG <stefan.tanase2121@gmail.com>
2024-01-10 13:40:09 +0200

    example get for brands

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BrandDTO } from "./types";
 
 export default function App() {
+  const [data, setData] = useState<BrandDTO[]>();
+  useEffect(() => {
+    const fetchBrands = async () => {
+      const response = await axios.get("/api/brand");
+      console.log(response.data);
+      setData(response.data);
+    };
+    fetchBrands();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        {data?.map((brand) => (
+          <tr>
+            <td>{brand.id}</td>
+            <td>{brand.name}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
