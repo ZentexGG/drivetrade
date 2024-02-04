392da4ca67fe943ae1891811197fdad2ee304547
ZentexGG <stefan.tanase2121@gmail.com>
2024-02-04 16:38:14 +0200

    loading now centered

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function loading() {
-  return <span className="loading loading-dots loading-lg"></span>;
+  return (
+    <center>
+      <span className="loading loading-dots loading-lg" />
+    </center>
+  );
 }
