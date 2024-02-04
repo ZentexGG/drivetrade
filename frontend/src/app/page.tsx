74be4158fd6ba0058b57c958d7c8661cefee2693
ZentexGG <stefan.tanase2121@gmail.com>
2024-02-04 16:39:00 +0200

    removed unnecessary imoport

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,4 @@
 import CarSearch from "@/components/CarSearch/CarSearch";
-import DataFetch from "@/components/DataFetch/DataFetch";
 import WelcomeText from "@/components/WelcomeText/WelcomeText";
 
 export default async function Home() {
